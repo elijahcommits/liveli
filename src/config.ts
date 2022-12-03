09ed37aebe71637a85e481c109897ef0ef2141c8
Elijah <14240674+elijahcommits@users.noreply.github.com>
2022-12-03 00:21:09 -0800

    change SITE_DESCRIPTION

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Liveli';
-export const SITE_DESCRIPTION = 'Live performances across the web!';
+export const SITE_DESCRIPTION = 'Musical gems, live from Earth!';
